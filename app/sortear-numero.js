4845b3767f78442a24eba3563f926abd463c27a2
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-06-14 16:44:49 -0300

    Manipulando menor e maior valor

--- app/sortear-numero.js
+++ app/sortear-numero.js
@@ -1,10 +1,19 @@
+const menorValor = 1;
+const maiorValor = 100;
+
+
 const numeroSecreto = gerarNumeroAleatorio()
 
 
 function gerarNumeroAleatorio(){
 
-  return parseInt(Math.random()*100)
+  return parseInt(Math.random() * maiorValor + 1 ) 
 
 
 
 }
+
+const elementoMenorValor = document.getElementById('menor-valor')
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
