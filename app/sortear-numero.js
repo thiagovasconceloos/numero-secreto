f0da5b0d11440b7393cceffbc6cdcae3baf3f886
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-06-16 16:36:17 -0300

    criando logica do jogo

--- app/sortear-numero.js
+++ app/sortear-numero.js
@@ -1,19 +1,15 @@
-const menorValor = 1;
-const maiorValor = 100;
-
-
+const menorValor = 1
+const maiorValor = 1000
 const numeroSecreto = gerarNumeroAleatorio()
 
-
-function gerarNumeroAleatorio(){
-
-  return parseInt(Math.random() * maiorValor + 1 ) 
-
-
-
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
 }
 
+console.log('Número Secreto:', numeroSecreto)
+
 const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
 const elementoMaiorValor = document.getElementById('maior-valor')
-elementoMenorValor.innerHTML = menorValor;
-elementoMaiorValor.innerHTML = maiorValor;
+elementoMaiorValor.innerHTML = maiorValor
